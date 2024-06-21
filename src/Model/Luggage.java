910d38a41e36390f302934e1dde6b2a74d287a50
Martin <martinjosejaime@gmail.com>
2024-06-21 12:58:02 -0300

    comento codigo que rompia

--- src/Model/Luggage.java
+++ src/Model/Luggage.java
@@ -15,18 +15,18 @@ public class Luggage<T>{
 
     public boolean isOverweight(int index){
         boolean response = false;
-        if(luggage.get(index).getClass().getSimpleName().equalsIgnoreCase("Suitcase")){
-            Suitcase suitcase = (Suitcase) luggage.get(index);
-            if (suitcase.getWeight()>24F){
-                response = true;
-            }
-        }
-        if (luggage.get(index).getClass().getSimpleName().equalsIgnoreCase("Backpack")){
-            Backpack backpack = (Backpack) luggage.get(index);
-            if (backpack.getWeight()>4F){
-                response = true;
-            }
-        }
+//        if(luggage.get(index).getClass().getSimpleName().equalsIgnoreCase("Suitcase")){
+//            Suitcase suitcase = (Suitcase) luggage.get(index);
+//            if (suitcase.getWeight()>24F){
+//                response = true;
+//            }
+//        }
+//        if (luggage.get(index).getClass().getSimpleName().equalsIgnoreCase("Backpack")){
+//            Backpack backpack = (Backpack) luggage.get(index);
+//            if (backpack.getWeight()>4F){
+//                response = true;
+//            }
+//        }
         return response;
     }
 
